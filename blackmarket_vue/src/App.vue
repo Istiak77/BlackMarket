33fcacbcb9fff8681a8c102297be05c875ea4df8
Istiak77 <ahmedpranto666@gmail.com>
2025-05-18 20:50:28 +0600

    setup

---
+++ blackmarket_vue/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbrand">
+        <router-link to="/" class="navbar-item"><strong>Black Market</strong></router-link>
+        
+        <a  class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/tshirt" class="navbar-item">T-Shirts</router-link>
+          <router-link to="/hoodie" class="navbar-item">Hoodies</router-link>
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light">Log In</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    
+</div>
+</template>
+
+<style lang="scss">
+@import '../node_modules/bulma';
+</style>
