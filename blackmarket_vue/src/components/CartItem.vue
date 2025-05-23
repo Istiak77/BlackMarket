d870a5e7f846117c26ea89ded830494a44029f22
Istiak77 <ahmedpranto666@gmail.com>
2025-05-23 10:48:22 +0600

    log in/out

--- blackmarket_vue/src/components/CartItem.vue
+++ blackmarket_vue/src/components/CartItem.vue
@@ -4,10 +4,10 @@
       <router-link :to="item.product.get_absolute_url">{{ item.product.name }}</router-link>
     </td>
     <td>Tk {{ item.product.price }}</td>
-    <td>
-      {{ item.quantity }}
-      <button class="button is-small is-rounded is-danger mx-1" @click="decrementQuantity(item)">-</button>
-      <button class="button is-small is-rounded is-success mx-1" @click="incrementQuantity(item)">+</button>
+    <td class="has-text-centered">
+      <button class="button is-small is-rounded-2 is-danger mx-1" @click="decrementQuantity(item)">-</button>
+      <span class="quantity-display has-text-weight-bold has-text-primary mx-2">{{ item.quantity }}</span>
+      <button class="button is-small is-rounded-2 is-success mx-1" @click="incrementQuantity(item)">+</button>
     </td>
     <td>Tk {{ getItemTotal(item).toFixed(2) }}</td>
     <td>
@@ -18,41 +18,51 @@
 
 <script>
 export default {
-    name: 'CartItem',
-    props: {
-        initialItem: Object
+  name: 'CartItem',
+  props: {
+    initialItem: Object
+  },
+  data() {
+    return {
+      item: this.initialItem
+    }
+  },
+  methods: {
+    getItemTotal(item) {
+      return item.quantity * item.product.price
     },
-    data() {
-        return {
-            item: this.initialItem
-        }
+    decrementQuantity(item) {
+      item.quantity -= 1
+
+      if (item.quantity === 0) {
+        this.$emit('removeFromCart', item)
+      }
+
+      this.updateCart()
     },
-    methods: {
-        getItemTotal(item) {
-            return item.quantity * item.product.price
-        },
-        decrementQuantity(item) {
-            item.quantity -= 1
+    incrementQuantity(item) {
+      item.quantity += 1
 
-            if (item.quantity === 0) {
-                this.$emit('removeFromCart', item)
-            }
-
-            this.updateCart()
-        },
-        incrementQuantity(item) {
-            item.quantity += 1
-
-            this.updateCart()
-        },
-        updateCart() {
-            localStorage.setItem('cart', JSON.stringify(this.$store.state.cart))
-        },
-        removeFromCart(item) {
-            this.$emit('removeFromCart', item)
-
-            this.updateCart()
-        },
+      this.updateCart()
     },
+    updateCart() {
+      localStorage.setItem('cart', JSON.stringify(this.$store.state.cart))
+    },
+    removeFromCart(item) {
+      this.$emit('removeFromCart', item)
+
+      this.updateCart()
+    },
+  },
 }
 </script>
+
+<style scoped>
+.quantity-display {
+  font-size: 1.1rem;
+  padding: 0 0.5em;
+  display: inline-block;
+  min-width: 1.5em;
+  text-align: center;
+}
+</style>
--- blackmarket_vue/src/router/index.js
+++ blackmarket_vue/src/router/index.js
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import store from '../store'
+
 import HomeView from '../views/HomeView.vue'
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
+import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
 
 const routes = [
   {
@@ -18,28 +22,41 @@ const routes = [
     name: 'about',
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
-      {
+  {
     path: '/sign-up',
     name: 'SignUp',
     component: SignUp
   },
-    {
+  {
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogIn
+  },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta:{
+      requireLogin: true
+    }
+  },
+  {
     path: '/search',
     name: 'Search',
     component: Search
   },
-      {
+  {
     path: '/cart',
     name: 'Cart',
     component: Cart
   },
   {
-    path:'/:category_slug/:product_slug',
+    path: '/:category_slug/:product_slug',
     name: 'Product',
     component: Product
   },
-    {
-    path:'/:category_slug',
+  {
+    path: '/:category_slug',
     name: 'Category',
     component: Category
   },
@@ -48,6 +65,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'LogIn', query: { to: to.path } });
+  } else {
+    next()
+  }
 })
 
 export default router
