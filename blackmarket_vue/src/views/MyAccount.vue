91faf6ec57c5044b0a2ec49c56ac510075002af8
Istiak77 <ahmedpranto666@gmail.com>
2025-05-25 12:29:27 +0600

    o auth fixed

--- blackmarket_vue/src/views/MyAccount.vue
+++ blackmarket_vue/src/views/MyAccount.vue
@@ -3,10 +3,45 @@
         <div class="columns is-multiline">
             <div class="column is-12">
                 <h1 class="title">My account</h1>
+                
+                <!-- User Info Section -->
+                <div class="box" v-if="userInfo">
+                    <div class="media">
+                        <div class="media-left">
+                            <figure class="image is-64x64">
+                                <img 
+                                    v-if="userInfo.picture" 
+                                    :src="userInfo.picture" 
+                                    alt="Profile picture"
+                                    class="is-rounded"
+                                >
+                                <span v-else class="icon is-large">
+                                    <i class="fas fa-user-circle fa-3x"></i>
+                                </span>
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4">{{ userInfo.name || username }}</p>
+                            <p class="subtitle is-6">{{ userInfo.email }}</p>
+                            <p v-if="isGoogleUser" class="tag is-info">
+                                <span class="icon">
+                                    <i class="fab fa-google"></i>
+                                </span>
+                                <span>Google Account</span>
+                            </p>
+                            <p v-else class="tag is-light">Regular Account</p>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div class="column is-12">
-                <button @click="logout()" class="button is-danger">Log out</button>
+                <button @click="logout()" class="button is-danger">
+                    <span class="icon">
+                        <i class="fas fa-sign-out-alt"></i>
+                    </span>
+                    <span>Log out</span>
+                </button>
             </div>
 
             <hr>
@@ -28,44 +63,94 @@ import axios from 'axios'
 
 export default {
     name: 'MyAccount',
-    components: {
-    },
     data() {
         return {
-            orders: []
+            orders: [],
+            userInfo: null,
+            username: localStorage.getItem('username') || '',
+            isGoogleUser: false
         }
     },
     mounted() {
         document.title = 'My account | Black Market'
-
+        this.getUserInfo()
         this.getMyOrders()
     },
     methods: {
+        async getUserInfo() {
+            try {
+                // Check for Google user data
+                const googleData = JSON.parse(localStorage.getItem('googleUser'))
+                if (googleData) {
+                    this.userInfo = {
+                        name: googleData.name,
+                        email: googleData.email,
+                        picture: googleData.picture
+                    }
+                    this.isGoogleUser = true
+                } else {
+                    // For regular users, fetch from your API if needed
+                    const response = await axios.get('/api/v1/users/me/')
+                    this.userInfo = {
+                        name: response.data.username,
+                        email: response.data.email
+                    }
+                }
+            } catch (error) {
+                console.error("Error fetching user info:", error)
+                // Fallback to basic info
+                this.userInfo = {
+                    name: this.username,
+                    email: ''
+                }
+            }
+        },
         logout() {
+            // Clear all auth data
             axios.defaults.headers.common["Authorization"] = ""
-
+            
             localStorage.removeItem("token")
             localStorage.removeItem("username")
             localStorage.removeItem("userid")
+            localStorage.removeItem("googleUser")
+            localStorage.removeItem("authToken")
 
             this.$store.commit('removeToken')
 
-            this.$router.push('/')
+            // Redirect to home with logout message
+            this.$router.push('/?logout=true')
         },
         async getMyOrders() {
             this.$store.commit('setIsLoading', true)
 
-            await axios
-                .get('/api/v1/orders/')
-                .then(response => {
-                    this.orders = response.data
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-
-            this.$store.commit('setIsLoading', false)
+            try {
+                const response = await axios.get('/api/v1/orders/')
+                this.orders = response.data
+            } catch (error) {
+                console.error("Error fetching orders:", error)
+                this.$toast.error("Failed to load orders")
+            } finally {
+                this.$store.commit('setIsLoading', false)
+            }
         }
     }
 }
 </script>
+
+<style scoped>
+.box {
+    margin-bottom: 2rem;
+}
+.tag {
+    margin-top: 0.5rem;
+}
+.media-content {
+    padding-left: 1rem;
+}
+.image.is-rounded img {
+    border-radius: 50%;
+}
+.button.is-danger .icon {
+    margin-right: 0.5em;
+}
+</style>
--- blackmarket_vue/src/views/SignUp.vue
+++ blackmarket_vue/src/views/SignUp.vue
@@ -1,268 +1,275 @@
 <template>
-    <div class="page-sign-up">
-        <div class="columns">
-            <div class="column is-4 is-offset-4">
-                <h1 class="title">Sign up</h1>
+  <div class="page-sign-up">
+    <div class="columns">
+      <div class="column is-4 is-offset-4">
+        <h1 class="title">Sign up</h1>
 
-                <form @submit.prevent="submitForm">
-                    <div class="field">
-                        <label>Username</label>
-                        <div class="control">
-                            <input type="text" class="input" v-model="username" />
-                        </div>
-                    </div>
-
-                    <div class="field">
-                        <label>Password</label>
-                        <div class="control">
-                            <input type="password" class="input" v-model="password" />
-                        </div>
-                    </div>
-
-                    <div class="field">
-                        <label>Repeat password</label>
-                        <div class="control">
-                            <input type="password" class="input" v-model="password2" />
-                        </div>
-                    </div>
-
-                    <div class="notification is-danger" v-if="errors.length">
-                        <p v-for="error in errors" :key="error">{{ error }}</p>
-                    </div>
-
-                    <div class="field">
-                        <div class="control">
-                            <button class="button is-dark">Sign up</button>
-                        </div>
-                    </div>
-
-                    <hr />
-
-                    Or <router-link to="/log-in">click here</router-link> to log in!
-                </form>
-
-                <hr />
-
-                <div>
-                    <button @click="loginWithGoogle" class="button is-success">
-                        Login with Google
-                    </button>
-                </div>
+        <form @submit.prevent="submitForm">
+          <div class="field">
+            <label>Username</label>
+            <div class="control">
+              <input type="text" class="input" v-model="username" />
             </div>
+          </div>
+
+          <div class="field">
+            <label>Password</label>
+            <div class="control">
+              <input type="password" class="input" v-model="password" />
+            </div>
+          </div>
+
+          <div class="field">
+            <label>Repeat password</label>
+            <div class="control">
+              <input type="password" class="input" v-model="password2" />
+            </div>
+          </div>
+
+          <div class="notification is-danger" v-if="errors.length">
+            <p v-for="error in errors" :key="error">{{ error }}</p>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <button class="button is-dark">Sign up</button>
+            </div>
+          </div>
+
+          <hr />
+
+          Or <router-link to="/log-in">click here</router-link> to log in!
+        </form>
+
+        <hr />
+
+        <div>
+          <button @click="showGoogleLogin" class="button is-success" :disabled="isGoogleLoading">
+            <span v-if="!isGoogleLoading">
+              <i class="fab fa-google"></i> Sign up with Google
+            </span>
+            <span v-else>
+              <i class="fas fa-spinner fa-spin"></i> Loading...
+            </span>
+          </button>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import axios from "axios";
-import { toast } from "bulma-toast";
+  import axios from "axios";
+  import { toast } from "bulma-toast";
 
-export default {
-  name: "SignUp",
-  data() {
-    return {
-      username: "",
-      password: "",
-      password2: "",
-      errors: [],
-      googleClientId: process.env.VUE_APP_GOOGLE_CLIENT_ID || "1000611960672-7c0qdquqmbu9v0jcmfht1uoe4fi7bh28.apps.googleusercontent.com",
-      isGoogleLoading: false
-    };
-  },
-  mounted() {
-    this.initializeGoogleAuth();
-  },
-  methods: {
-    async initializeGoogleAuth() {
-      this.isGoogleLoading = true;
-      this.errors = [];
-      
-      try {
-        await this.loadGoogleScript();
-        
-        if (window.google?.accounts?.id) {
-          const config = {
+  export default {
+    name: "SignUp",
+    data() {
+      return {
+        username: "",
+        password: "",
+        password2: "",
+        errors: [],
+        googleClientId: process.env.VUE_APP_GOOGLE_CLIENT_ID || "1000611960672-7c0qdquqmbu9v0jcmfht1uoe4fi7bh28.apps.googleusercontent.com",
+        isGoogleLoading: false,
+        googleAuthInitialized: false
+      };
+    },
+    mounted() {
+      this.loadGoogleScript();
+    },
+    methods: {
+      async loadGoogleScript() {
+        if (window.google?.accounts?.id) return;
+
+        return new Promise((resolve) => {
+          const script = document.createElement("script");
+          script.src = "https://accounts.google.com/gsi/client";
+          script.async = true;
+          script.defer = true;
+          script.onload = () => {
+            this.initializeGoogleAuth();
+            resolve();
+          };
+          script.onerror = () => {
+            this.errors = ["Failed to load Google sign-in. Please try the manual form."];
+          };
+          document.head.appendChild(script);
+        });
+      },
+
+      initializeGoogleAuth() {
+        if (this.googleAuthInitialized) return;
+
+        try {
+          window.google.accounts.id.initialize({
             client_id: this.googleClientId,
-            callback: this.handleGoogleCredentialResponse,
+            callback: (response) => this.handleGoogleCredentialResponse(response),
             ux_mode: 'popup',
             auto_select: false,
-            hosted_domain: window.location.hostname === 'localhost' ? 'localhost' : undefined
-          };
-
-          // Add FedCM flag only if supported
-          if (window.google.accounts.id.useFedcm) {
-            config.use_fedcm_for_prompt = true;
-          }
-
-          window.google.accounts.id.initialize(config);
-
-          // Backward-compatible prompt handling
-          window.google.accounts.id.prompt(notification => {
-            // Check for both old and new API versions
-            if (typeof notification.isNotDisplayed === 'function') {
-              // New FedCM API
-              if (notification.isNotDisplayed()) {
-                const reason = notification.getNotDisplayedReason?.() || 'unknown_reason';
-                this.handlePromptError(reason);
-              }
-            } else if (typeof notification.isNotDisplayed === 'boolean') {
-              // Old API
-              if (notification.isNotDisplayed || notification.isSkippedMoment) {
-                this.errors = ["Google sign-in popup was blocked. Please allow popups."];
-              }
-            }
+            context: 'signup',
+            itp_support: true,
+            use_fedcm_for_prompt: true
           });
+          this.googleAuthInitialized = true;
+        } catch (error) {
+          console.error("Google auth initialization failed:", error);
+          this.errors = ["Google sign-in configuration error. Please use the manual form."];
         }
-      } catch (error) {
-        console.error("Google auth initialization failed:", error);
-        this.errors = ["Failed to initialize Google sign-in."];
-      } finally {
-        this.isGoogleLoading = false;
-      }
-    },
+      },
 
-    handlePromptError(reason) {
-      const errorMap = {
-        "user_cancel": "Sign-in was canceled",
-        "unregistered_origin": "This domain is not authorized",
-        "invalid_client": "Invalid client configuration",
-        "missing_client_id": "Missing client ID",
-        "unknown_reason": "Unknown error occurred"
-      };
-      
-      this.errors = [errorMap[reason] || "Google sign-in failed. Please try again."];
-    },
+      showGoogleLogin() {
+        this.isGoogleLoading = true;
+        this.errors = [];
 
-    async loadGoogleScript() {
-      return new Promise((resolve) => {
-        if (window.google?.accounts?.id) {
-          resolve();
+        if (!window.google?.accounts?.id) {
+          this.errors = ["Google sign-in not ready. Please try again."];
+          this.isGoogleLoading = false;
           return;
         }
 
-        const script = document.createElement("script");
-        script.src = "https://accounts.google.com/gsi/client";
-        script.async = true;
-        script.defer = true;
-        script.onload = resolve;
-        script.onerror = () => {
-          this.errors = ["Failed to load Google sign-in. Please refresh."];
-        };
-        document.head.appendChild(script);
-      });
-    },
+        window.google.accounts.id.prompt(notification => {
+          this.isGoogleLoading = false;
 
-    async handleGoogleCredentialResponse(response) {
-      try {
-        this.isGoogleLoading = true;
-        this.errors = [];
-        
-        const res = await axios.post("/api/v1/auth/google/", {
-          access_token: response.credential,
-          id_token: response.credential
-        });
+          if (notification.isNotDisplayed()) {
+            const reason = notification.getNotDisplayedReason();
+            console.log("Prompt not displayed:", reason);
 
-        const token = res.data.key || res.data.access_token;
-        if (!token) {
-          throw new Error("No authentication token received");
-        }
-
-        // Store token and set default auth header
-        localStorage.setItem("authToken", token);
-        axios.defaults.headers.common["Authorization"] = `Token ${token}`;
-
-        toast({
-          message: "Logged in successfully via Google!",
-          type: "is-success",
-          dismissible: true,
-          pauseOnHover: true,
-          duration: 2000,
-          position: "bottom-right",
-        });
-
-        this.$router.push("/");
-      } catch (error) {
-        console.error("Google auth error:", error.response?.data || error.message);
-        
-        let errorMessage = "Google login failed. Please try again.";
-        if (error.response?.data?.error) {
-          errorMessage = error.response.data.error;
-        } else if (error.response?.status === 400) {
-          errorMessage = "Invalid request to server.";
-        } else if (error.response?.status === 401) {
-          errorMessage = "Authentication failed. Invalid credentials.";
-        }
-        
-        this.errors = [errorMessage];
-      } finally {
-        this.isGoogleLoading = false;
-      }
-    },
-
-    submitForm() {
-      this.errors = [];
-
-      if (this.username === "") {
-        this.errors.push("The username is missing");
-      }
-      if (this.password === "") {
-        this.errors.push("The password is too short");
-      }
-      if (this.password !== this.password2) {
-        this.errors.push("The passwords don't match");
-      }
-
-      if (!this.errors.length) {
-        const formData = {
-          username: this.username,
-          password: this.password,
-        };
-
-        axios
-          .post("/api/v1/users/", formData)
-          .then(() => {
-            toast({
-              message: "Account created, please log in!",
-              type: "is-success",
-              dismissible: true,
-              pauseOnHover: true,
-              duration: 2000,
-              position: "bottom-right",
-            });
-            this.$router.push("/log-in");
-          })
-          .catch((error) => {
-            if (error.response) {
-              for (const property in error.response.data) {
-                this.errors.push(
-                  `${property}: ${error.response.data[property]}`
-                );
-              }
-              console.error("Signup error:", error.response.data);
-            } else if (error.message) {
-              this.errors.push("Something went wrong. Please try again");
-              console.error("Signup error:", error);
+            if (reason === 'invalid_client') {
+              this.errors = ["Google sign-in is currently unavailable. Please use the manual form."];
+            } else if (reason === 'suppressed_by_user') {
+              this.errors = ["You previously dismissed Google sign-in. Please refresh to try again."];
+            } else {
+              this.errors = ["Couldn't load Google sign-in. Please try the manual form."];
             }
+          }
+        });
+      },
+
+      async handleGoogleCredentialResponse(response) {
+        try {
+          this.isGoogleLoading = true;
+          this.errors = [];
+
+          const res = await axios.post("/api/v1/auth/google/", {
+            access_token: response.credential,
+            id_token: response.credential
           });
+
+          const token = res.data.key || res.data.access_token;
+          if (!token) {
+            throw new Error("No authentication token received");
+          }
+
+          localStorage.setItem("authToken", token);
+          axios.defaults.headers.common["Authorization"] = `Token ${token}`;
+
+          toast({
+            message: "Logged in successfully via Google!",
+            type: "is-success",
+            dismissible: true,
+            pauseOnHover: true,
+            duration: 2000,
+            position: "bottom-right",
+          });
+
+          this.$router.push("/");
+        } catch (error) {
+          console.error("Google auth error:", error.response?.data || error.message);
+          this.errors = ["Google login failed. Please try the manual form."];
+        } finally {
+          this.isGoogleLoading = false;
+        }
+      },
+
+      submitForm() {
+        this.errors = [];
+
+        if (this.username === "") {
+          this.errors.push("The username is missing");
+        }
+        if (this.password === "") {
+          this.errors.push("The password is too short");
+        }
+        if (this.password !== this.password2) {
+          this.errors.push("The passwords don't match");
+        }
+
+        if (!this.errors.length) {
+          const formData = {
+            username: this.username,
+            password: this.password,
+          };
+
+          axios
+            .post("/api/v1/users/", formData)
+            .then(() => {
+              toast({
+                message: "Account created, please log in!",
+                type: "is-success",
+                dismissible: true,
+                pauseOnHover: true,
+                duration: 2000,
+                position: "bottom-right",
+              });
+              this.$router.push("/log-in");
+            })
+            .catch((error) => {
+              if (error.response) {
+                for (const property in error.response.data) {
+                  this.errors.push(
+                    `${property}: ${error.response.data[property]}`
+                  );
+                }
+                console.error("Signup error:", error.response.data);
+              } else if (error.message) {
+                this.errors.push("Something went wrong. Please try again");
+                console.error("Signup error:", error);
+              }
+            });
+        }
       }
     }
-  }
-};
+  };
 </script>
 
 <style scoped>
-    .button.is-success {
-        background-color: #4285F4;
-        border-color: transparent;
-        color: white;
-    }
+  .page-sign-up {
+    padding: 20px;
+  }
 
-    .button.is-success:hover {
-        background-color: #357ABD;
-    }
+  .button.is-success {
+    background-color: #4285F4;
+    border-color: transparent;
+    color: white;
+    width: 100%;
+    padding: 1em;
+    font-weight: 500;
+  }
 
-    .button.is-success[disabled] {
-        opacity: 0.7;
-        cursor: not-allowed;
-    }
+  .button.is-success:hover {
+    background-color: #357ABD;
+  }
+
+  .button.is-success[disabled] {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  .fa-google {
+    margin-right: 8px;
+  }
+
+  .fa-spinner {
+    margin-right: 8px;
+  }
+
+  .notification.is-danger {
+    margin-top: 10px;
+    padding: 10px;
+  }
+
+  hr {
+    margin: 1.5rem 0;
+  }
 </style>
